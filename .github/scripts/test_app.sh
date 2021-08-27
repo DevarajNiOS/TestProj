449a8c835a0d440c632ac603d59415c40e2b91ec
Archana <Archu>
2021-08-27 16:24:03 +0530

    testing

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -4,4 +4,4 @@ set-x
 set -eo pipefail
 
 xcodebuild clean build -allowProvisioningUpdates -workspace TestProject.xcworkspace -scheme TestProject -destination 'generic/platform=iOS' CODE_SIGN_IDENTITY="Apple Development" CODE_SIGNING_REQUIRED=NO ONLY_ACTIVE_ARCH=NO
-clean test | xcpretty
+#clean test | xcpretty
