1fece2ba5c1d390745d88ed8823bb588a383bbf9
Archana <Archu>
2021-08-27 17:16:50 +0530

    added echo command

--- .github/scripts/decrypt_secrets.sh
+++ .github/scripts/decrypt_secrets.sh
@@ -8,13 +8,13 @@ set -eo pipefail
 
 
 gpg -d -o './certsNew.p12'  --pinentry-mode=loopback --passphrase $CERTS_ENCRYPTION_PWD  './certsNew.p12.gpg'
+echo $gpg 
 
 gpg -d -o './provisioning.tar.gz.gpg' --pinentry-mode=loopback --passphrase $PROVISION_ENCRYPTION_PWD './provisioning.tar.gz.gpg'
-
-
+echo $gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles/*
-
+echo $PWD
 for PROVISION in `ls ./*.mobileprovision`
 
         do
@@ -25,25 +25,13 @@ for PROVISION in `ls ./*.mobileprovision`
 
         done
 
-
-
-
-
-
-
 security create-keychain -p "" build.keychain
 security import './certsNew.p12' -t agg -k ~/Library/Keychains/build.keychain -P "" -A
-
-
-
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 
 security default-keychain -s ~/Library/Keychains/build.keychain
 
 security unlock-keychain -p "" ~/Library/Keychains/build.keychain
-
-
-
 
 security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
